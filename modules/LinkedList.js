a814c97511587afdd9c758a658334b6f19e2280f
Sima Margulis Davidi <simamarg@gmail.com>
2018-01-18 10:35:40 +0200

    added tests to all functions

--- modules/LinkedList.js
+++ modules/LinkedList.js
@@ -1,95 +1,94 @@
 function Node(data) {
-  this.data = data;
-  this.next = null;
+    this.data = data;
+    this.next = null;
 };
 
 function LinkedList() {
-  this._length = 0;
-  this.head = null;
+    this._length = 0;
+    this.head = null;
 };
 
-LinkedList.prototype.consoleLogAll = function() {
+LinkedList.prototype.consoleLogAll = function () {
 
-  // Assign the currentNode to the first element in the array
-  var currentNode = this.head;
-
-  // loop until the last element (null)
-  while (currentNode != null) {
-    console.log(currentNode.data);
-    currentNode = currentNode.next;
-  }
-};
-LinkedList.prototype.AddToTheBeginning = function(data) {
-  var newNode = new Node(data);
-  // Reference the new item to the existing first element.
-  newNode.next = this.head;
-  // Place the new node at the beginning of the list.
-  this.head = newNode;
-  //increase the length
-  this._length++;
-};
-
-LinkedList.prototype.DeleteItem = function(data) {
-
-  // Assign the currentNode to the first element in list
-  var currentNode = this.head;
-
-  // If the item to delete is the head it's fairly easy 
-  if (currentNode && currentNode.data == data) {
-    this.head = currentNode.next;
-    this._length--;
-  } else {
-    //if it's not we'll loop over the rest of the list
-    //and when there is a match then skip the item
-    while (currentNode != null) {
-      if (currentNode.next && currentNode.next.data === data) {
-        currentNode.next = currentNode.next.next;
-        this._length--;
-        return; // Exit the function when you done
-      }
-      currentNode = currentNode.next; //Go to next item
-    }
-  }
-};
-
-
-LinkedList.prototype.AddToTheEnd = function(value) {
-  var node = new Node(value);
-  var currentNode = this.head;
-  // 1st use-case: an empty list 
-  if (!currentNode) {
-    this.head = node;
-    this._length++;
-    return node;
-  }
-  // 2nd use-case: a non-empty list
-  while (currentNode.next) {
-    currentNode = currentNode.next;
-  }
-  //Add the item
-  currentNode.next = node;
-  this._length++;
-  return node;
-};
-
-
-LinkedList.prototype.RemoveItemByIndex = function(index) {
-  // 1st use-case: the item to delete is the first item 
-  if (index === 0) {
-    this.head = this.head.next;
-    this._length--;
-  } else {
     // Assign the currentNode to the first element in the array
     var currentNode = this.head;
-    //Loop to the wanted position
-    for (var i = 1; i < index; i++) {
-      //Go to next item
-      currentNode = currentNode.next;
+
+    // loop until the last element (null)
+    while (currentNode != null) {
+        console.log(currentNode.data);
+        currentNode = currentNode.next;
     }
-    // Skip the item
-    currentNode.next = currentNode.next.next;
-    this._length--;
-  }
 };
 
-module.exports = {LinkedList:LinkedList,Node:Node}
+LinkedList.prototype.AddToTheBeginning = function (data) {
+    var newNode = new Node(data);
+    // Reference the new item to the existing first element.
+    newNode.next = this.head;
+    // Place the new node at the beginning of the list.
+    this.head = newNode;
+    //increase the length
+    this._length++;
+};
+
+LinkedList.prototype.DeleteItem = function (data) {
+
+    // Assign the currentNode to the first element in list
+    var currentNode = this.head;
+
+    // If the item to delete is the head it's fairly easy 
+    if (currentNode && currentNode.data == data) {
+        this.head = currentNode.next;
+        this._length--;
+    } else {
+        //if it's not we'll loop over the rest of the list
+        //and when there is a match then skip the item
+        while (currentNode != null) {
+            if (currentNode.next && currentNode.next.data === data) {
+                currentNode.next = currentNode.next.next;
+                this._length--;
+                return; // Exit the function when you done
+            }
+            currentNode = currentNode.next; //Go to next item
+        }
+    }
+};
+
+LinkedList.prototype.AddToTheEnd = function (value) {
+    var node = new Node(value);
+    var currentNode = this.head;
+    // 1st use-case: an empty list 
+    if (!currentNode) {
+        this.head = node;
+        this._length++;
+        return node;
+    }
+    // 2nd use-case: a non-empty list
+    while (currentNode.next) {
+        currentNode = currentNode.next;
+    }
+    //Add the item
+    currentNode.next = node;
+    this._length++;
+    return node;
+};
+
+LinkedList.prototype.RemoveItemByIndex = function (index) {
+    // 1st use-case: the item to delete is the first item 
+    if (index === 0) {
+        this.head = this.head.next;
+        this._length--;
+    } else {
+        // Assign the currentNode to the first element in the array
+        var currentNode = this.head;
+        //Loop to the wanted position
+        for (var i = 1; i < index; i++) {
+            //Go to next item
+            currentNode = currentNode.next;
+        }
+        // Skip the item
+        currentNode.next = currentNode.next.next;
+        this._length--;
+    }
+};
+
+module.exports = { LinkedList: LinkedList, Node: Node }
--- spec/LinkedListSpec.js
+++ spec/LinkedListSpec.js
@@ -1,10 +1,77 @@
-var {LinkedList,Node} = require('../modules/LinkedList');
+var { LinkedList, Node } = require('../modules/LinkedList');
 
-describe("CreateList", function() {
-  it("Should create an object with an array inside", function() {
-    var list = new LinkedList();
+describe("CreateList", function () {
+    it("Should create an object with an array inside", function () {
+        var list = new LinkedList();
 
-    expect(list._length).toBe(0);
-    expect(list.head).toBe(null);
-  });
+        expect(list._length).toBe(0);
+        expect(list.head).toBe(null);
+    });
+});
+
+describe("consoleLogAll", function () {
+    it("Should print the linked list data", function () {
+        let list = new LinkedList();
+        list.AddToTheBeginning(3);
+        list.AddToTheBeginning(4);
+        console.log = jasmine.createSpy("log");
+        list.consoleLogAll();
+        expect(console.log).toHaveBeenCalledWith(4);
+        expect(console.log).toHaveBeenCalledWith(3);
+    });
+});
+
+
+describe("AddToTheBeginning", function () {
+    it("Should create a node with the given data and add it to the beginning of the list", function () {
+        var list = new LinkedList();
+        list.AddToTheBeginning(3);
+        list.AddToTheBeginning(4);
+
+        expect(list._length).toBe(2);
+        expect(list.head.data).toBe(4);
+        expect(list.head.next.data).toBe(3);
+        expect(list.head.next.next).toBe(null);
+    });
+});
+
+describe("DeleteItem", function () {
+    it("Should find a node with the given data and delete it from the list", function () {
+        var list = new LinkedList();
+        list.AddToTheBeginning(3);
+        list.AddToTheBeginning(4);
+        list.DeleteItem(3);
+
+        expect(list._length).toBe(1);
+        expect(list.head.data).toBe(4);
+        expect(list.head.next).toBe(null);
+    });
+});
+
+describe("AddToTheEnd", function () {
+    it("Should create a node with the given data and add it to the end of the list", function () {
+        var list = new LinkedList();
+        list.AddToTheEnd(3);
+        list.AddToTheEnd(4);
+
+        expect(list._length).toBe(2);
+        expect(list.head.data).toBe(3);
+        expect(list.head.next.data).toBe(4);
+        expect(list.head.next.next).toBe(null);
+    });
+});
+
+describe("RemoveItemByIndex", function () {
+    it("Should find a node in a given index and delete it from the list", function () {
+        var list = new LinkedList();
+        list.AddToTheBeginning(3);
+        list.AddToTheBeginning(4);
+        list.AddToTheEnd(5);
+        list.RemoveItemByIndex(1);
+
+        expect(list._length).toBe(2);
+        expect(list.head.data).toBe(4);
+        expect(list.head.next.data).toBe(5);
+        expect(list.head.next.next).toBe(null);
+    });
 });
